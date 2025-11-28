62922959ae91b1d79600da9ab674bf524892259a
Dushyna <olgavnv777@gmail.com>
2025-11-28 15:06:40 +0100

    sync- monitor

--- src/ait/elevator/task/Truck.java
+++ src/ait/elevator/task/Truck.java
@@ -3,6 +3,7 @@ package ait.elevator.task;
 import ait.elevator.model.Elevator;
 
 public class Truck implements Runnable{
+    private static final Object monitor = new Object();
     private  int nRace;
     private int capacity;
     private Elevator elevator;
@@ -16,7 +17,7 @@ public class Truck implements Runnable{
     @Override
     public void run() {
         for (int i = 0; i <nRace ; i++) {
-            synchronized ("mutex") {
+            synchronized (monitor) {
                 elevator.add(capacity);
             }
         }
