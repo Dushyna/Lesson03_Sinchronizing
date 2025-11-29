3cd0a1b206aa6fdfc53c0cdae33fd89e1b553aec
Dushyna <olgavnv777@gmail.com>
2025-11-29 15:32:05 +0100

    elevator2 init

--- src/ait/elevator/task/ElevatorAppl.java
+++ src/ait/elevator/task/ElevatorAppl.java
@@ -4,30 +4,30 @@ import ait.elevator.model.Elevator;
 
 public class ElevatorAppl {
 
-    private static final int N_TRUCK=10000;
-    private static final int N_RACES= 10;
-    private static final int CAPACITY= 20;
+    private static final int N_TRUCK = 10000;
+    private static final int N_RACES = 10;
+    private static final int CAPACITY = 20;
 
 
     public static void main(String[] args) throws InterruptedException {
         Elevator elevator1 = new Elevator("V.I. Lenin");
         Elevator elevator2 = new Elevator("Ukraina");
-        Truck[] trucks=new Truck[N_TRUCK];
+        Truck[] trucks = new Truck[N_TRUCK];
         for (int i = 0; i < trucks.length; i++) {
-            trucks[i]= new Truck(N_RACES,CAPACITY, elevator1, elevator2);
+            trucks[i] = new Truck(N_RACES, CAPACITY, elevator1, elevator2);
 
         }
         Thread[] threads = new Thread[trucks.length];
         for (int i = 0; i < threads.length; i++) {
-            threads[i]= new Thread(trucks[i]);
+            threads[i] = new Thread(trucks[i]);
             threads[i].start();
 
         }
-        for(Thread thread :  threads){
+        for (Thread thread : threads) {
             thread.join();
         }
-        System.out.println("CurrentVolume  elevator1 " + elevator1.getCurrentVolume());
-        System.out.println("CurrentVolume  elevator2 " + elevator2.getCurrentVolume());
+        System.out.println("CurrentVolume " + elevator1.getName() + " " + elevator1.getCurrentVolume());
+        System.out.println("CurrentVolume " + elevator2.getName() + " " + elevator2.getCurrentVolume());
 
     }
 }
--- src/ait/elevator/task/Truck.java
+++ src/ait/elevator/task/Truck.java
@@ -2,8 +2,10 @@ package ait.elevator.task;
 
 import ait.elevator.model.Elevator;
 
-public class Truck implements Runnable{
-    private  int nRace;
+public class Truck implements Runnable {
+    private static final Object monitor1 = new Object();
+    private static final Object monitor2 = new Object();
+    private int nRace;
     private int capacity;
     private Elevator elevator1;
     private Elevator elevator2;
@@ -17,9 +19,13 @@ public class Truck implements Runnable{
 
     @Override
     public void run() {
-        for (int i = 0; i <nRace ; i++) {
-            elevator1.add(capacity/2);
-            elevator2.add(capacity/2);
+        for (int i = 0; i < nRace; i++) {
+            synchronized (monitor1) {
+                elevator1.add(capacity / 2);
+            }
+            synchronized (monitor2) {
+                elevator2.add(capacity / 2);
+            }
 
         }
     }
